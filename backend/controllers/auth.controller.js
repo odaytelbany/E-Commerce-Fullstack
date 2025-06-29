aedc8942662cf13616420afc7cf2e81d46c7d1d0
odaytelbany <oday0595814990@gmail.com>
2025-06-29 21:56:32 +0300

    Edit - Add Tokens to register controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,32 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+const generateToken = (userId) => {
+  const accessToken = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '15m'});
+  const refreshToken = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'});
+  return { accessToken, refreshToken };
+}
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(`refresh_token:${userId}`, refreshToken, 'EX', 60 * 60 * 24 * 7); // Store for 7 days
+}
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  })
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 15 minutes
+  })
+}
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -20,7 +47,16 @@ export const register = async (req, res) => {
       email,
       password: hashedPassword,
     }); 
-    res.status(201).json({ data: user, message: "User created successfully" });
+    const {accessToken, refreshToken} = generateToken(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({ user: {
+      _id: user._id,
+      name: user.name, 
+      email: user.email,
+      role: user.role
+    }, message: "User created successfully" });
   } catch (error) {
     console.log("Error in Register controller:", error);
     res.status(500).json({
