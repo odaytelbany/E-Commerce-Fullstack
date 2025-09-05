795afdd7667466edc6847512f9fbe1bab610c23a
odaytelbany <oday0595814990@gmail.com>
2025-09-05 10:49:58 +0300

    Feature - create the product list page

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -5,7 +5,9 @@ import axios from "../lib/axios";
 export const useProductStore = create((set) => ({
     products: [],
     loading: false,
+
     setProducts: (products) => set({products}),
+
     createProduct: async (productData) => {
         set({loading: true});
         try{
@@ -20,5 +22,43 @@ export const useProductStore = create((set) => ({
             toast.error(error?.response?.data?.message || "Error creating product");
             set({loading: false});
         }
-    }
+    },
+
+    fetchAllProducts: async () => {
+        set({loading: true});
+        try {
+            const response = await axios.get("/products");
+            set({products: response.data, loading: false})
+        } catch (error) {
+            set({error: "Faild to fetch products", loading: false})
+            toast.error(error.response?.data?.message || "Error fetching products");
+        }
+    },
+
+    toggleFeaturedProduct: async (productId) => {
+        set({loading: true});
+        try {
+            const response = await axios.patch(`/products/${productId}`);
+            set((prevProducts) => ({
+                products: prevProducts.products.map((product) => product._id === productId ? {...product, isFeatured: response.data.isFeatured} : product),
+                loading: false
+            }))
+        } catch (error) {
+            set({loading: false});
+            toast.error(error.response?.data?.message || "Error updating product")
+        }
+    },
+
+    deleteProduct: async (productId) => {
+        try {
+            await axios.delete(`/products/${productId}`);
+            set((prevProducts) => ({
+                products: prevProducts.products.filter((product) => product._id !== productId),
+                loading: false
+            }))
+        } catch (error) {
+            set({loading: false});
+            toast.error(error.response?.data?.message || "Error updating product")
+        }
+    },
 }));
