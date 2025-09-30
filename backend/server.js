aa13ce8ba82a4211bcb79c3d883bea655fa64e9a
odaytelbany <oday0595814990@gmail.com>
2025-09-30 09:49:45 +0300

    Fix - fix server.js cod

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.status(path.join(__dirname, "/frontend/dist")))
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
     app.get("*", (req, res) => {
         res.sendFile(path.relative(__dirname, "frontend", "dist", "index.html"))
